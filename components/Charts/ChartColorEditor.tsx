cc8b3b90df0c7dbf18a1e80e2eb5c7016204f71a
Andrian Fathulloh <andrianfathulloh05@gmail.com>
2025-06-09 20:17:16 +0700

    bug fixing

--- components/Charts/ChartColorEditor.tsx
+++ components/Charts/ChartColorEditor.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Card } from '@/components/ui/card';
-import { ColorPicker } from '../UI/ColorPicker';
+import { ColorPicker } from '../ui/ColorPicker';
 import { ChartData, ChartConfig } from '@/types';
 
 interface ChartColorEditorProps {
